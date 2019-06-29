8f6cc8c9c4f694588d3575ab3cbd76dd17bdbc45
Juliet <chepngetichjuliet@gmail.com>
2019-06-29 16:27:22 +0300

    configures model

--- app/models.py
+++ app/models.py
@@ -49,11 +49,12 @@ class Review:
 
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):
