a6e8ce126bf179c079d0329ff705a2eb06e9a216
Juliet <chepngetichjuliet@gmail.com>
2019-06-29 16:58:12 +0300

    adds the correct content to views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,8 @@
 from flask import render_template
 from . import auth
-from flask_login import LoginManager
+from flask_login import login_required
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
+
 
 
 def create_app(config_name):
@@ -12,7 +10,7 @@ def create_app(config_name):
     #Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    login_manager.init_app(app)
+    
 
 
 @auth.route('/login')
