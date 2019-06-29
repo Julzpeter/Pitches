78c3ee65550949a92e62024e97df3f0580dfb686
Juliet <chepngetichjuliet@gmail.com>
2019-06-29 17:15:38 +0300

    removes a line of code in auth/views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from flask_login import LoginManager
-from .forms import RegistrationForm
+from flask_login import login_user
+from .forms import RegistrationForm,LoginForm
 from .. import db
 from ..models import User
 
@@ -31,3 +31,16 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html', registration_form=form)
+
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "pitch login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
