0ab639c2eeb8414b04e7bf10434ea914f9167c46
Juliet <chepngetichjuliet@gmail.com>
2019-06-29 16:24:19 +0300

    adds a login decorator to  views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,18 @@ from flask import render_template
 from . import auth
 from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+    #....
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
 
 @auth.route('/login')
 def login():
