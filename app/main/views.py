102bbf6e3993715c040443b5df0d5c38eda88b36
Juliet <chepngetichjuliet@gmail.com>
2019-06-29 20:52:10 +0300

    adda view function that will handle an Update request

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,40 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..request import get_movies,get_movie,search_movie
-from .forms import ReviewForm
+from .forms import ReviewForm, UpdateProfile
 from ..models import Review, User
 from flask_login import login_required
+from .. import db
 
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
+
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
 
 
 # Views
@@ -70,11 +99,4 @@ def new_review(id):
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
 
-@main.route('/user/<uname>')
-def profile(uname):
-    user = User.query.filter_by(username=uname).first()
 
-    if user is None:
-        abort(404)
-
-    return render_template("profile/profile.html", user=user)
