32642156176bde64a63106829fbbe3a026e1a59d
Juliet <chepngetichjuliet@gmail.com>
2019-06-30 13:29:59 +0300

    creates tables pitch and adds content

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 from ..models import User
 from .forms import LoginForm, RegistrationForm
-from ..email import mail_message
+# from ..email import mail_message
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -26,13 +26,13 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data, password=form.password.data)
+        # user = User(email=form.email.data,
+        #             username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to watchlist",
-                     "email/welcome_user", user.email, user=user)
-        title = "New Account"
+        # mail_message("Welcome to watchlist",
+        #              "email/welcome_user", user.email, user=user)
+        # title = "New Account"
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', registration_form=form)
 
--- app/models.py
+++ app/models.py
@@ -34,29 +34,21 @@ class User(UserMixin, db.Model):
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-
-
-   
-
-class Pitch(UserMixin, db.Model):
-    __tablename__ = 'pitch'
+class Pitch(UserMixin,db.Model):
+    __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
-    pitch_title = db.Column(db.String)
-    pitch_body = db.Column(db.String)
-    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    category = db.Column(db.String(255))
-   ## pitch = db.relationship("Comment", backref="user_pitch", lazy="dynamic")
-    posted_by = db.Column(db.String)
+    pitch_content = db.Column(db.String())
+    pitch_category = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    def save_review(self):
-        db.session.add(self)
-        db.session.commit()
+    
+   
+
+  
 
    
 
-    def __repr__(self):
-        return f'User {self.username}'
+   
 
 
 
@@ -64,50 +56,10 @@ class Pitch(UserMixin, db.Model):
 
 
 
-class Movie:
-    '''
-    Movie class to define Movie Objects
-    '''
-
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
 
 
 
-class Review:
 
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
 
 
 
