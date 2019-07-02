cd3114d59b99c4560d3ba987f349f6fd496ec0a2
Juliet <chepngetichjuliet@gmail.com>
2019-07-02 11:36:42 +0300

    procfile

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MOVIE_API_KEY=c88e788def814ce79a95882b1a99ef54
-export SECRET_KEY=1234567890
+export SQLALCHEMY_DATABASE_URI='DATABASE_URL'
+export SECRET_KEY='12345'
 export MAIL_USERNAME='chepngetichjuliet@gmail.com'
 export MAIL_PASSWORD='kapsiya12338jul'
 
--- tests/test_user.py
+++ tests/test_user.py
@@ -6,14 +6,30 @@ from app.models import User
 class TestUser(unittest.TestCase):
 
     def setUp(self):
-        self.new_fev= User(username="fev",password="123",email="fev@gmail.com",id=1)
+        self.user_fev= User(username="fev",password="123",email="fev@gmail.com",id=1)
+
+    def tearDown(self):
+        User.query.delete()
+
+        def test_check_instance_variables(self):
+            self.assertEquals(self.new_fev.id, 1)
+            self.assertEquals(self.new_fev.username,'fev')
+            self.assertEquals(self.new_fev.bio, '')
+            self.assertEquals(self.new_fev.email, 'fev@gmail.com')
+            self.assertEquals(self.new_fev.profile_pic_path, '')
+
+       
+
 
     def test_password_setter(self):
-        self.assertTrue(self.new_user.password_hash is not None)
+        self.assertTrue(self.user.password_hash is not None)
 
     def test_no_access_password(self):
             with self.assertRaises(AttributeError):
-                self.new_user.password
+                self.user.password
 
     def test_password_verification(self):
-            self.assertTrue(self.new_user.verify_password('123'))
+            self.assertTrue(self.user.verify_password('123'))
+
+if __name__ == "__main__":
+    unittest.main()
