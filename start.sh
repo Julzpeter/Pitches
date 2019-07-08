2a0df782c4d9570ff85e993ae4d844b707f9f758
Julzpeter <48478895+Julzpeter@users.noreply.github.com>
2019-07-08 13:52:33 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 export SECRET_KEY='12345'
 export MAIL_USERNAME='chepngetichjuliet@gmail.com'
-export MAIL_PASSWORD='kapsiya12338jul'
+
 
 python3.7 manage.py server
